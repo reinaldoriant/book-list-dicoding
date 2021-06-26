9db8742ab65f87033316dcdaa40fd6f0474aba6f
Reinaldo <reinaldo.riant@kompas.com>
2021-06-26 16:57:54 +0700

    first commit

---
+++ app/src/main/java/com/ruangaldo/booklist/MainActivity.kt
@@ -0,0 +1,11 @@
+gipackage com.ruangaldo.booklist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
