7f54e5a846142d4d77ee46d97439801171403843
Reinaldo <reinaldo.riant@kompas.com>
2021-07-04 22:06:04 +0700

    ! add story books

--- app/src/main/java/com/ruangaldo/booklist/data/data/StoryBook.kt
+++ app/src/main/java/com/ruangaldo/booklist/data/data/StoryBook.kt
@@ -49,6 +49,49 @@ object StoryBook {
                 "9789674310646",
                 R.drawable.img_angin_matahari,
                 "Siri ini menyajikan 6 buah buku cerita teladan untuk menerapkan nilai moral. Sesuai dibaca oleh kanak-kanak berumur 4 hingga 6 tahun. Pengunaan perkataan dan ayat yang mudah digunakan secara berulang untuk proses pembacaan yang berkesan. Pemenggalan perkataan mengikut suku kata untuk memudahkan penyebutan perkataan."
+            ), Book(
+                "25 Kisah Nabi & Rasul",
+                "Aan Wulandari U.",
+                "Bentang Belia",
+                "05 October 2017",
+                "9786024301378",
+                R.drawable.img_25_nabi,
+                "Tahukah kamu, bagaimana kisah Nabi Adam si manusia pertama, kisah Nabi Ibrahim yang tak mempan dibakar api, serta kisah hijrah yang menakjubkan Nabi Muhammad Saw.? Di manakah para nabi dan rasul diturunkan Allah di muka bumi ini? Yuk, temukan semua kisahnya dalam buku ini. Pssst, buku ini dilengkapi dengan trivia persebaran nabi dan rasul, lho. Temukan halaman berlogo khusus, lalu pindai dengan smart pen-mu, ya. Kamu akan menemukan kejutan menariknya! 25 Kisah Nabi dan Rasul adalah buku yang tepat untuk anak saleh dan salehah seperti kalian. Selamat membaca!"
+            ), Book(
+                "12 Ilmuwan Muslim yang Terkenal di Dunia",
+                "Izzah Annisa",
+                "Bentang Belia",
+                "29 Juni 2018",
+                "9786024303198",
+                R.drawable.img_12_ilmuwan,
+                "Tahukah kamu bahwa teropong bintang, kamera, robot, serta pesawat terbang  pertama di dunia diciptakan oleh para ilmuwan Muslim? Yuk, kita berkenalan dengan Al-Battani sang penemu teropong bintang, Abbas bin Firnas sang penemu pesawat terbang, Al-Khawarizmi sang penemu angka nol pertama di dunia, dan masih banyak lagi ilmuwan Muslim lainnya di dalam buku ini!"
+            ),
+            Book(
+                "The Best of Fabel Al-Qur'an",
+                "Adji Annisa Zakiandini",
+                "WahyuMedia",
+                "01 January 2016",
+                "9786023780655",
+                R.drawable.img_the_fabel_quran,
+                "Buku ini memuat kisah-kisah hewan yang disebutkan di Al-Qur'an. Terdapat juga hikmah kisah dan pelajaran yang bisa kita petik di dalamnya."
+            ),
+            Book(
+                "Cerita Teladan Anak Sholeh",
+                "Rauhiyatul Jannah",
+                "WahyuMedia",
+                "01 January 2014",
+                "9789797958589",
+                R.drawable.img_cerita_teladan,
+                "Mempelajari hadis dapat dilakukan sejak dini. Orang tua/pedamping dapat menyampaikan hadis melalui cerita. Dalam buku ini, terdapat 20 kisah menarik yang dilengkapi dengan hadits pilihan. "
+            ),
+            Book(
+                "Kisah & Makna 99 Asmaul Husna Untuk Anak",
+                "Mei Tatazakka",
+                "AnakKita",
+                "01 January 2012",
+                "9786029003215",
+                R.drawable.img_kisah_makna,
+                "Tahukah adik-adik, makna dari setiap nama dalam 99 Asmaul Husna? Jika belum tahu, kita akan belajar bersama-sama agar adik-adik tahu nama dan makna dari Asmaul Husna.Buku ini menjelaskan satu per satu secara runut 99 Asmaul Husna mulai dari Ar-Rahmaan, Ar-Rahiim, sampai nama yang ke-99, yaitu Ash-Shabuur. Di dalamnya selain ada penjelasan makna yang terkandung juga penggambaran dalam kisah-kisah yang terjadi ketika zaman kenabian maupun saat ini. Sehingga, adik-adik akan lebih mudah memahami makna dari tiap nama Allah tersebut."
             )
         )
 }
